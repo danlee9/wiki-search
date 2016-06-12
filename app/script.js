b6693a681f84d26b89f1f47f55bf5ef3c525ba97
unknown <dan.dh.lee@gmail.com>
2016-06-11 21:43:21 -0700

    Add search functionality for browsers that do not support search event

--- app/script.js
+++ app/script.js
@@ -1,34 +1,72 @@
+
+var el = document.createElement('input');
+
+el.setAttribute('search', 'return;');
+
+// tests if browser supports search event. will return 'function' if it does and undefined if it does not
+var canSearch = typeof el.onsearch;
+
+
 $(function() {
 	var $search = $('#search'),
-			$link = $('.search-link');
-			$results = $('#results');
+			$link = $('.search-link'),
+			$results = $('#results'),
 			search;
-	$search.on('search', function() {
+	
+	var wikiSearch = function() {
 		search = $search.val();
 		$.ajax({
-		  url: '//en.wikipedia.org/w/api.php',
-		  data: { action: 'opensearch', format: 'json', search: search },
-		  dataType: 'jsonp',
-		  success: function (x) {
-		  	if ($search.val() == '') return;
-		    $link.each(function(i) {
-		    	var $this = $(this);
-		    	$this.find('h2').text(x[1][i]);
-		    	$this.find('p').text(x[2][i]);
-		    	$this.attr('href', x[3][i]);
-		    });
-		    $results.animate({
-		    	opacity: 1,
-		    	top: 0
-		    }, 500);
-		  }
+			url: '//en.wikipedia.org/w/api.php',
+			data: { action: 'opensearch', format: 'json', search: search },
+			dataType: 'jsonp',
+			success: function (x) {
+				if ($search.val() == '') return;
+				$link.each(function(i) {
+					var $this = $(this);
+					$this.find('h2').text(x[1][i]);
+					$this.find('p').text(x[2][i]);
+					$this.attr('href', x[3][i]);
+				});
+				$results.animate({
+					opacity: 1,
+					top: 0
+				}, 500);
+			}
 		});
+	};
+	
+	
+	if (canSearch) {
+		$search.on('search', function() {
+			wikiSearch();
+		});
+	} else {
+		$search.on('keypress', function(e) {
+			if (e.which == 13) {
+				wikiSearch();
+			}
+		});	
+	}
+	
+	// clear search entries when erasing text in input
+	$search.on('keypress', function(e) {
+		if (e.which == 8) {
+			$this = $(this);
+			setTimeout(function() {
+				if ($this.val() == '') {
+					$results.css({
+						opacity: 0,
+						top: '9999px'
+					});
+				}
+			},100);
+		}
 	});
-
+	
+	// clears search entries when clicking 'x' on search input
 	$search.on('mouseup', function() {
 		var $this = $(this);
 		var oldVal = $this.val();
-		console.log(oldVal);
 		if (oldVal == '') return;
 
 		var newVal;
